7435efe7ce205ba3840b449e0fb3128456679fb1
24DP1KBerg <a24016kb@rvt.lv>
2025-09-16 14:39:47 +0300

    Average of three numbers

--- src/main/java/rvt/adding_numbers.java
+++ src/main/java/rvt/adding_numbers.java
@@ -8,11 +8,12 @@ public class adding_numbers
     {
         Scanner scanner = new Scanner(System.in);
         
-        int first = 2;
-        int second = 4;
+        
+        int firstd = 2;
+        int secondd = 4;
 
-        System.out.println(first + second); // prints 6
-        System.out.println(2 + second - first - second); // prints 0
+        System.out.println(firstd + secondd); // prints 6
+        System.out.println(2 + secondd - firstd - secondd); // prints 0
         System.out.println();
 
         //User writes 2 number and prints out their sum
@@ -24,6 +25,40 @@ public class adding_numbers
         int answer = number + number2;
 
         System.out.println(number + " + " + number2 + " = " + answer);
+        
+        
+        //Division
+        /*
+         *  int first = 3;
+            int second = 2;
+            double result = first / second;        Skat. prioritāšu lapā
+            System.out.println(result);
+
+            atbilde: 1.0
+         */
+
+            System.out.println();
+            int first = 3;
+            int second = 2;
+            //double result = Double.valueOf(first) / Double.valueOf(second);       1. variants
+            //double result = (double) first / second;      1. variants
+            double result = 1.0 * first / second;
+            System.out.println(result);
+
+            //average of 3 numbers
+
+        System.out.println("write your 1st number");
+        int averagenumber = scanner.nextInt();
+        System.out.println("write your 2nd number");
+        int averagenumber2 = scanner.nextInt();
+        System.out.println("write your 3rd number");
+        int averagenumber3 = scanner.nextInt();
+
+        double sum = averagenumber + averagenumber2 + averagenumber3; // var arī ar int kā sum un pēc tam double average = (double) / 3;
+
+        double average = sum / 3;
+
+        System.out.println("Average: " + average);
 
     }   
 }
