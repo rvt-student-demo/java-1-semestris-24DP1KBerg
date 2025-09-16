f1a0a12db201274d14eb87cd422cc6211bf757a9
24DP1KBerg <a24016kb@rvt.lv>
2025-09-16 14:01:15 +0300

    Addition formula

--- src/main/java/rvt/adding_numbers.java
+++ src/main/java/rvt/adding_numbers.java
@@ -13,7 +13,17 @@ public class adding_numbers
 
         System.out.println(first + second); // prints 6
         System.out.println(2 + second - first - second); // prints 0
-        
+        System.out.println();
+
+        //User writes 2 number and prints out their sum
+        System.out.println("write your 1st number");
+        int number = scanner.nextInt();
+        System.out.println("write your 2nd number");
+        int number2 = scanner.nextInt();
+
+        int answer = number + number2;
+
+        System.out.println(number + " + " + number2 + " = " + answer);
 
     }   
 }
