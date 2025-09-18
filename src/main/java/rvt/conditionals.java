dce9260962351a26b3f63ee15eaa68f599a69404
24DP1KBerg <a24016kb@rvt.lv>
2025-09-18 12:29:02 +0300

    Pievienoju odd or even uzdevumu

--- src/main/java/rvt/conditionals.java
+++ src/main/java/rvt/conditionals.java
@@ -6,27 +6,18 @@ public class conditionals {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Give points [0-100]:\n");
-        int points = scanner.nextInt();
+        System.out.print("Give number:\n");
+        int number = scanner.nextInt();
         
-        if (points < 0) {
-            System.out.println("Grade: impossible!");
-        } else if (points <= 49) {
-            System.out.println("Grade: failed");
-        } else if (points <= 59) {
-            System.out.println("Grade: 1");
-        } else if (points <= 69) {
-            System.out.println("Grade: 2");
-        } else if (points <= 79) {
-            System.out.println("Grade: 3");
-        } else if (points <= 89) {
-            System.out.println("Grade: 4");
-        } else if (points <= 100) {
-            System.out.println("Grade: 5");
-        } else {
-            System.out.println("Grade: incredible!");
+        if (number % 2 == 0) 
+        {
+            System.out.println("Number is even");    
         }
-        
+        else{
+            System.out.println("Number is odd");
+
+        }
+
         scanner.close();
     }
 }
