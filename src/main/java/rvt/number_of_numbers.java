ed75034fe8d764bf2cbf788e1269d23c464a9339
24DP1KBerg <a24016kb@rvt.lv>
2025-09-26 08:46:50 +0300

    Pievieonoju numbers of numbers uzdevumu

---
+++ src/main/java/rvt/number_of_numbers.java
@@ -0,0 +1,33 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class number_of_numbers
+{
+
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        int numbers = 0;
+
+        while (true) 
+        {
+            
+            System.out.println("Give a number");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) 
+            {
+                break;    
+            }
+
+            numbers = numbers + 1;
+
+        }
+        System.out.println("Number of numbers: " + numbers);
+
+        scanner.close();
+    }
+
+}
