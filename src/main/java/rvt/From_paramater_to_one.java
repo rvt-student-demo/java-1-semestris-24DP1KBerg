2c63e50bd174174bcbc47921fc920265d4e9f617
24DP1KBerg <a24016kb@rvt.lv>
2025-09-30 13:59:29 +0300

    From paramater to one faili izpildiju divisible by three uzdevumu

--- src/main/java/rvt/From_paramater_to_one.java
+++ src/main/java/rvt/From_paramater_to_one.java
@@ -5,15 +5,19 @@ public class From_paramater_to_one
 
     public static void main(String[] args)
     {
-        printUntilNumber(5);
+        divisibleByThreeInRange(2, 9);
 
     }
 
 
-    public static void printUntilNumber(int numOfTimes) {
-        int i = 0;
-        while (i < numOfTimes + 1) {
-            System.out.println(i);
+    public static void divisibleByThreeInRange(int starting, int ending) {
+        int i = starting;
+        while (i < ending + 1) {
+            if (i % 3 == 0) 
+            {
+                System.out.println(i);
+            }
+            
             i++;
         }
     }
