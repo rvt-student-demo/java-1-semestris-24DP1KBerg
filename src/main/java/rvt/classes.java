db87183b17fd58fa3bd01d4db301e4657e71de75
24DP1KBerg <a24016kb@rvt.lv>
2025-11-12 09:17:29 +0200

    Statistics (4 parts)

--- src/main/java/rvt/classes.java
+++ src/main/java/rvt/classes.java
@@ -1,28 +1,38 @@
 package rvt;
 
-
+import java.util.Scanner;
 
 public class classes 
 {
     
     public static void main(String[] args)
     {
-
+        Scanner scanner = new Scanner(System.in);
         Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
+        while (true)
+        {   
+        
+           int numbers = scanner.nextInt();
+           if (numbers == -1) 
+           {
+                 break;
+           } 
+           statistics.addNumber(numbers);
+        }
+        
         System.out.println("Count: " + statistics.getCount());
         System.out.println("Sum: " + statistics.sum());
         System.out.println("Average: " + statistics.average());
+        System.out.println("Sum of even numbers: " + statistics.sumEven());
+        System.out.println("Sum of odd numbers: " + statistics.sumOdd());
 
     }
 
     public static class Statistics {
         private int count;
         private int sum;
-  
+        private int even;
+        private int odd;
 
         public Statistics() {
             count = 0;
@@ -32,6 +42,12 @@ public class classes
         public void addNumber(int number) {
             count++;
             sum = sum + number;
+            if(number % 2 == 0){
+                even = even + number;
+            }
+            else{
+                odd = odd + number;
+            }
         }
     
         public int getCount() { 
@@ -40,6 +56,14 @@ public class classes
 
         public int sum() {
             return sum;
+        } 
+        public int sumEven(){
+            return even;
+
+        }
+
+        public int sumOdd(){
+            return odd;
         }
 
         public double average() {
