4de54d8d149d6c1056a2cc4dd67ce7f720a7b911
Kristaps Aleksis Bergs <a240165kb@rvt.lv>
2025-12-11 23:23:21 +0200

    Sum of array

--- src/main/java/rvt/SumOfArray.java
+++ src/main/java/rvt/SumOfArray.java
@@ -4,7 +4,7 @@ public class SumOfArray {
 
     public static void main(String[] args) {
 
-        int[] numbers = {5, 1, 3, 4, 2};
+        int[] numbers = {5, 1, 3, 4, 2}; // create an array
 
         System.out.println("Sum: " + sumOfNumbersInArray(numbers));
     }
