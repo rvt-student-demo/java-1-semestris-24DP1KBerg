b5833647f4bd6a26c47306b21b5affcf08bd9a71
Kristaps Aleksis Bergs <a240165kb@rvt.lv>
2025-12-11 23:26:39 +0200

    Immutable Box

--- src/main/java/rvt/Box.java
+++ src/main/java/rvt/Box.java
@@ -10,7 +10,7 @@ public class Box {
     private final double height;
     private final double length;
 
-    // Constructor: Box(width, height, length)
+    // Constructor: Box(width, height, length) s
     public Box(double width, double height, double length) {
         this.width = width;
         this.height = height;
